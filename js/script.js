67607160a9bd76ce6b1df1b8f57588f5e0f06784
STHyeon <toby2009@naver.com>
2020-10-13 16:32:14 +0900

    Add modal  & Conform to Rule

--- js/script.js
+++ js/script.js
@@ -20,12 +20,12 @@ $(function () {
                 spaceBetween: 20,
             },
             // when window width is >= 480px
-            480: {
+            630: {
                 slidesPerView: 2,
                 spaceBetween: 20,
             },
             // when window width is >= 640px
-            640: {
+            768: {
                 slidesPerView: 3,
                 spaceBetween: 30,
             },
@@ -33,6 +33,7 @@ $(function () {
     });
 
     // fixed header navigation bar
+
     const header = document.querySelector(".nav_box");
     const sticky = header.offsetTop - 70;
 
@@ -44,24 +45,27 @@ $(function () {
         }
     }
 
-    window.onscroll = function () {
-        navScroll();
-    };
+    // change current scroll position
+    function CurrentScroll() {
+        var doc_scroll = $(document).scrollTop();
+        var fixed_height = Number($(".nav_box").height()),
+            tabcon01_top = Number($("#about").offset().top - fixed_height),
+            tabcon02_top = Number($("#skill").offset().top - fixed_height),
+            tabcon03_top = Number($("#project").offset().top - fixed_height),
+            tabcon04_top = Number($("#call").offset().top - fixed_height);
 
-    // smooth scorll
-    $(".nav_box li").click(function (e) {
-        var target = $(this).children("a");
-        var element = target.attr("href");
-        $(".nav_box li a").removeClass("active");
-        target.addClass("active");
-
-        $("html, body").animate(
-            {
-                scrollTop: $(element).offset().top,
-            },
-            800,
-        );
-    });
+        if (doc_scroll < tabcon01_top) {
+            $(".nav_box li").removeClass("active").eq(0).addClass("active");
+        } else if (doc_scroll > tabcon01_top && doc_scroll < tabcon02_top) {
+            $(".nav_box li").removeClass("active").eq(1).addClass("active");
+        } else if (doc_scroll > tabcon02_top && doc_scroll < tabcon03_top) {
+            $(".nav_box li").removeClass("active").eq(2).addClass("active");
+        } else if (doc_scroll > tabcon03_top && doc_scroll < tabcon04_top - 600) {
+            $(".nav_box li").removeClass("active").eq(3).addClass("active");
+        } else if (doc_scroll > tabcon04_top - 600) {
+            $(".nav_box li").removeClass("active").eq(4).addClass("active");
+        }
+    }
 
     // skill navi
     $(".area_nav .nav_link").click(function () {
@@ -72,22 +76,85 @@ $(function () {
     });
 
     // skill percent animation
-    $(window).scroll(
-        $.throttle(500, function () {
-            var sct = $(window).scrollTop();
-            var skill = $(".additional_box").offset();
+    function SkillPercent() {
+        var sct = $(window).scrollTop();
+        var skill = $("#box_type01").offset();
 
-            if (sct > skill.top) {
-                $(".chart_bar span").addClass("animation"); // 한 번만
-            } else {
-                // $(".chart_bar span").removeClass("animation"); // 여러 번
-            }
-        }),
-    );
+        if (sct > skill.top) {
+            $(".chart_bar span").addClass("animation"); // 한 번만
+        } else {
+            // $(".chart_bar span").removeClass("animation"); // 여러 번
+        }
+    }
+
+    function open_layer() {
+        $(".openlayer").each(function () {
+            $(this).on("click", function (e) {
+                var return_focus = this;
+                var layer_id = $(this).attr("id");
+                $(".curtain").remove();
+                show_layer(layer_id);
+                $("#layer_" + layer_id)
+                    .attr("tabindex", 0)
+                    .focus();
+                $(".closelayer").on("click", function () {
+                    hide_layer(layer_id, return_focus);
+                });
+            });
+        });
+    }
+
+    function show_layer(layer_id) {
+        $("body").css("overflow-y", "hidden");
+        $("body").append("<div class='curtain'></div>");
+        $(".layer_wrap").show();
+        $(".layer").hide();
+        $("#layer_" + layer_id).fadeIn();
+    }
+
+    function hide_layer(layer_id, return_focus) {
+        if (layer_id == "") {
+            $(".layer").hide();
+        } else {
+            $("#layer_" + layer_id).hide();
+            $(return_focus).focus();
+        }
+        $(".layer_wrap").hide();
+        $(".curtain").remove();
+        $("body").css("overflow-y", "scroll");
+    }
+
+    // smooth scorll
+    $(".nav_box li").click(function () {
+        var target = $(this).children("a");
+        var element = target.attr("href");
+        $(".nav_box li").removeClass("active");
+        target.parent().addClass("active");
+
+        $("html, body").animate(
+            {
+                scrollTop: $(element).offset().top,
+            },
+            800,
+        );
+    });
 
     // project navi
     $(".project_nav li").click(function () {
         $(".project_nav li").removeClass("is_checked");
         $(this).addClass("is_checked");
     });
+
+    open_layer();
+
+    $(window).on("scroll", function () {
+        navScroll();
+        CurrentScroll();
+    });
+
+    $(window).scroll(
+        $.throttle(500, function () {
+            SkillPercent();
+        }),
+    );
 });
